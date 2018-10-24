9eba8561d4760ec4be735a392451b16212f322c5
Karoliina Koivu <karoliinakoivu@gmail.com>
2018-10-25 01:28:20 +0300

    testi muutos jotta travis fails

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -131,7 +131,7 @@ public class VarastoTest {
         Varasto alkuSaldollinenVarasto = new Varasto(10, 20);
         double saatuMaara = alkuSaldollinenVarasto.getSaldo();
         
-        assertEquals(10, saatuMaara, vertailuTarkkuus);    
+        assertEquals(9, saatuMaara, vertailuTarkkuus);    
     }
     
     @Test
